a147df4627cf02a67998c5726727648374d2816a
Ymt <tuanit2304@gmail.com>
2020-02-25 14:11:14 +0700

    button

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,15 +2,21 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var a : String = ""
-        a = "a"
-        a.toInt()
-        val b : String = "Ymt"
+        button1.setOnClickListener {
+            Toast.makeText(this, "Xin chao", Toast.LENGTH_SHORT).show()
+        }
     }
+
+//    fun Youclicked(view: View) {
+//        Toast.makeText(this, "bandaclick", Toast.LENGTH_SHORT).show()
+//    }
 }
