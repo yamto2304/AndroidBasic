6c81236b064c6eb2a765a71baf7cca0535c87e93
Ymt <tuanit2304@gmail.com>
2020-03-16 10:22:39 +0700

    timepicker and numpicker

--- app/src/main/java/com/example/testactivity/MainActivity.kt
+++ app/src/main/java/com/example/testactivity/MainActivity.kt
@@ -1,27 +1,43 @@
 package com.example.testactivity
 
-import android.content.Intent
-import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.view.View
 import android.widget.NumberPicker
 import android.widget.TextView
+import android.widget.TimePicker
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-
+    //private var tv2: TextView? = null
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var tv : TextView = findViewById(R.id.tvId)
+        var tv1 : TextView = findViewById(R.id.tv1Id)
         var np : NumberPicker = findViewById(R.id.npId)
 
         //bat su kien o number picker cho textview
-        np.setOnValueChangedListener { picker, oldVal, newVal -> tv!!.text = newVal.toString() }
+        np.setOnValueChangedListener { picker, oldVal, newVal -> tv1!!.text = newVal.toString() }
         np.maxValue = 100
         np.minValue = 0
+
+
+        var tv2 : TextView = findViewById(R.id.tv2Id)
+        val tp : TimePicker = findViewById(R.id.tpId)
+        val h = tp.currentHour
+        val m = tp.currentMinute
+        val tm = StringBuilder()
+        tm.append("$h h $m m")
+        tv2!!.text = tm
+        //bat su kien time picker cho textview
+        tp.setOnTimeChangedListener { view, hour, minute ->
+            val tm = StringBuilder()
+            tm.append("$hour h $minute m ")
+            tv2!!.text = tm
+        }
     }
 }
 
